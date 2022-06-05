f299163e7377587b85de2930f275fc813090b746
Carlos Miranda <miranda.bouzas@gmail.com>
2022-06-05 14:44:07 +0200

    migrations

---
+++ database/migrations/glofox/2022_06_05_120909_create_bookings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->string('member_name');
+            $table->dateTime('date');
+            $table->foreignId('class_id')->constrained()
+                ->cascadeOnUpdate()
+                ->restrictOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
