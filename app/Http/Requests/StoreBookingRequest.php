ed98e3613e1cadae1e5758fdca1d33966f83aa4b
Carlos Miranda <miranda.bouzas@gmail.com>
2022-06-12 00:18:03 +0200

    booking form

---
+++ app/Http/Requests/StoreBookingRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBookingRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'member_name' => 'required|string',
+            'date' => 'required|date',
+            'lesson_id' => 'required|integer'
+        ];
+    }
+}
